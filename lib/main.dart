215ae71ce58f7fc9503a8da46c9bfe0771ec139e
Mehmet Güler <mhmetglr.q@gmail.com>
2024-03-04 23:54:02 +0300

    Remove unused files and configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:quran/quran.dart' as quran;
+import 'package:flutter_quran_app/my_app.dart';
 
 void main() {
-  runApp(const MaterialApp(home: QuranExample()));
-}
-
-class QuranExample extends StatefulWidget {
-  const QuranExample({Key? key}) : super(key: key);
-
-  @override
-  State<QuranExample> createState() => _QuranExampleState();
-}
-
-class _QuranExampleState extends State<QuranExample> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: PageView.builder(
-          itemCount: 604,
-          itemBuilder: (context, index) {
-            return index == 0
-                ? const Center(child: Text("Bismillahirrahmanirrahim"))
-                : Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Center(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  "${quran.getSurahNameTurkish(index)} Suresi || $index",
-                                  style: const TextStyle(fontSize: 20),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            for (var i = 0; i < quran.getVerseCount(index); i++)
-                              Text(
-                                "${i + 1}.Ayet: ${quran.getVerseTranslation(index, i + 1, translation: quran.Translation.trSaheeh)}\n",
-                                style: const TextStyle(fontSize: 20),
-                                textAlign: TextAlign.justify,
-                              ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-          },
-        ),
-      ),
-    );
-  }
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
