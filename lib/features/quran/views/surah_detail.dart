d42afc8e6de278648d7ca15fe7b403389500f274
Mehmet Güler <mhmetglr.q@gmail.com>
2024-03-09 20:17:01 +0300

    Refactor SurahDetail widget and add SurahInfoItem widget

--- lib/features/quran/views/surah_detail.dart
+++ lib/features/quran/views/surah_detail.dart
@@ -1,11 +1,9 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quran_app/config/extensions/context_extension.dart';
-import 'package:flutter_quran_app/config/items/app_colors.dart';
-import 'package:flutter_quran_app/config/utility/enum/svg_enum.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:quran/quran.dart' as quran;
 
+import '../widgets/surah_info_item.dart';
 import '../widgets/verse_item.dart';
 
 class SurahDetail extends StatefulWidget {
@@ -40,67 +38,8 @@ class _SurahDetailState extends State<SurahDetail> {
           padding: context.paddingAllDefault,
           child: Column(
             children: [
-              Material(
-                elevation: 5,
-                borderRadius: BorderRadius.circular(10),
-                child: AspectRatio(
-                  aspectRatio: 1.5,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        colors: [
-                          AppColors.darkGreen,
-                          AppColors.lightGreen,
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: context.paddingAllDefault,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          quran.getSurahNameTurkish(widget.surahIndex),
-                          style: context.textTheme.labelMedium?.copyWith(
-                            fontSize: context.dynamicHeight(0.04),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          quran.getSurahNameEnglish(widget.surahIndex),
-                          style: context.textTheme.labelMedium?.copyWith(
-                            fontSize: context.dynamicHeight(0.032),
-                          ),
-                        ),
-                        const Divider(
-                          color: AppColors.whiteColor,
-                          endIndent: 50,
-                          indent: 50,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "${quran.getPlaceOfRevelation(widget.surahIndex)} ",
-                              style: context.textTheme.labelMedium?.copyWith(
-                                fontSize: context.dynamicHeight(0.02),
-                              ),
-                            ),
-                            Text(
-                              "◉ ${quran.getVerseCount(widget.surahIndex)} Verses",
-                              style: context.textTheme.labelMedium?.copyWith(
-                                fontSize: context.dynamicHeight(0.02),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SvgPicture.asset(SvgConstants.basmala.getSvg)
-                      ],
-                    ),
-                  ),
-                ),
+              SurahInfoItem(
+                surahIndex: widget.surahIndex,
               ),
               ListView.builder(
                 shrinkWrap: true,
